6de6dc6416f9540751a00fbdecc0648096617609
niwantha33 <niwantha33@gmail.com>
2023-01-28 19:31:21 +0000

    added hour_12hrs_format

--- starter/js/script.js
+++ starter/js/script.js
@@ -30,35 +30,54 @@ let get_working_dh = function () {
 
     function get_days() {
         // splitting the string to separate day and 24 hour
-        let _day = luxon.DateTime.now().toFormat('EEE, HH').split(',')
-        
+        let _day = luxon.DateTime.now().toFormat('EEE, HH:a').split(',')
+        console.log(_day)
         workDaysHours = {
             // check the day 
             dayCat: ['Sat', 'Sun'].includes(_day[0]) ? 'weekend' : 'week',
+
             day: _day[0], // day
-            hour:_day[1], // 24hrs
+
+            get hour_12hrs_format() {
+                let _t = _day[1].split(':') // split the "19: PM"
+                let _hour = Number(_t[0])
+
+                let tmp_hour = 0;
+
+                // convert the 24hrs to 12 hours 
+                if (_hour > 12) {
+
+                    tmp_hour = _hour -12; // 
+
+                }else{
+                    tmp_hour = _hour; 
+                }
+               
+                return tmp_hour+ _t[1]; 
+
+            }, // 24hrs
 
             // get the current hour using getter 
-            get workingHour(){                
+            get workingHour() {
                 return this.hour
-            } 
+            }
         }
         return workDaysHours;
     };
-    
+
     return get_days();
 }();
 
-let set_inline_blocks_for_hours = function (){
+let set_inline_blocks_for_hours = function () {
 
-    
+
 }
 
 $(document).ready(function () {
     // display current time 
     updateTime();
 
-    console.log(get_working_dh.workingHour)
+    console.log(get_working_dh.hour_12hrs_format)
 
 
 })
