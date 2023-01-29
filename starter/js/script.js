96ac8fb9907a46bd220668483b4989428646f162
niwantha33 <niwantha33@gmail.com>
2023-01-29 20:08:36 +0000

    fixed #6

--- starter/js/script.js
+++ starter/js/script.js
@@ -1,239 +1,228 @@
-/*
-@brief: update time and display the time 
-@param: N/A
-@return: N/A
-*/
 
+// wait to fully load
+$(document).ready(function () {
 
-/*
-@brief: get working hours and days 
-@param: N/A
-@return: N/A
-*/
+    /*
+    @brief: get working hours and days 
+    @param: N/A
+    @return: N/A
+    */
+    let getWorkDaysHours = function () {
 
+        // create object to handle working days and hours 
+        let workDaysHours = new Map();
 
-let get_working_dh = function () {
+        let getCurrentDayAndTime = function () { // inner function 
+            // splitting the string to separate day and 24 hour
+            let _day = luxon.DateTime.now().toFormat('EEE, HH:a').split(',');
 
-    // create object to handle working days and hours 
-    let workDaysHours = new Map();
+            workDaysHours = {
+                // check the day 
+                dayCat: ['Sat', 'Sun'].includes(_day[0]) ? 'weekend' : 'week',
 
-    let get_days = function () {
-        // splitting the string to separate day and 24 hour
-        let _day = luxon.DateTime.now().toFormat('EEE, HH:a').split(',');
+                get day() { return _day[0] }, // day
 
-        workDaysHours = {
-            // check the day 
-            dayCat: ['Sat', 'Sun'].includes(_day[0]) ? 'weekend' : 'week',
+                get hour_12hrs_format() {
 
-            get day() { return _day[0] }, // day
+                    let _t = _day[1].split(':') // split the "19: PM"
 
-            get hour_12hrs_format() {
+                    let _hour = Number(_t[0]);
 
-                let _t = _day[1].split(':') // split the "19: PM"
+                    let tmp_hour = 0;
 
-                let _hour = Number(_t[0]);
+                    // convert the 24hrs hour to 12 
+                    if (_hour > 12) {
 
-                let tmp_hour = 0;
+                        tmp_hour = _hour - 12; // 
 
-                // convert the 24hrs hour to 12 
-                if (_hour > 12) {
+                    } else {
+                        tmp_hour = _hour;
+                    }
 
-                    tmp_hour = _hour - 12; // 
+                    return tmp_hour + _t[1];
 
-                } else {
-                    tmp_hour = _hour;
-                }
+                },
+            }
+            return workDaysHours;
+        }();
 
-                return tmp_hour + _t[1];
-
-            },
-        }
-        return workDaysHours;
+        return getCurrentDayAndTime;
     }();
 
-    return get_days;
-}();
+    let createTimeBlocks = function () {
 
-let set_time_blocks = function () {
+        let time_ = new Array(10);
 
-    let time_ = new Array(10);
+        time_ = [
+            "7AM",
+            "8AM",
+            "9AM",
+            "10AM",
+            "11AM",
+            "12AM",
+            "6PM",
+            "7PM",
+            "8PM",
+            "4PM",
+            "5PM",
+        ]
 
-    time_ = [
-        "7AM",
-        "8AM",
-        "9AM",
-        "10AM",
-        "11AM",
-        "12AM",
-        "6PM",
-        "7PM",
-        "8PM",
-        "4PM",
-        "5PM",
-    ]
+        let color_class = ['past', 'present', 'future'];
 
-    let color_class = ['past', 'present', 'future'];
-   
-    let container = $('.container');
+        let container = $('.container');
 
-    // delete all the children 
-    if (container.children().length > 0) {
+        // delete all the children 
+        if (container.children().length > 0) {
 
-        container.empty();
-
-    }
-
-
-    for (let i = 0; i < time_.length; i++) {
-        let idx = -1; // get the index of the time_
-        let time_label = 0;
-        // console.log(get_working_dh.hour_12hrs_format)
-
-        if (time_.includes(get_working_dh.hour_12hrs_format)) {
-
-            idx = time_.indexOf(get_working_dh.hour_12hrs_format);
-        }
-
-        // set the color of the rows 
-        if (i < idx) {
-            time_label = color_class[0];
-
-        } else if (i == idx) {
-            time_label = color_class[1];
-
-        } else if (i > idx) {
-            time_label = color_class[2];
-        }
-
-        let row = $("<div/>", {
-            class: "row no-gutters",
-            id: "row_animate"
-        }).appendTo(container);
-
-        $("<label/>", {
-            class: "hour",
-            for: "hour",
-            id: `label-${i}`,
-            text: `${time_[i]}`
-
-        }).appendTo(row);
-
-        $("<textarea/>", {
-            class: `${time_label} description`,
-            id: `${i}`,
-            name: `${i}`
-
-        }).appendTo(row);
-
-        $("<button/>", {
-            class: `saveBtn ${i}`,
-            id: `${i}`,
-            html: `<i class="bi bi-save"></i>`
-
-        }).appendTo(row);
-
-    }
-
-    try {
-        let pst_data = persist_data();
-
-        if (pst_data !== null) {
-
-            pst_data.forEach((element, k) => {
-
-                let idx_ = time_.indexOf(element.time);
-
-                // console.log(luxon.DateTime.now().toLocaleString(), element.day)
-
-                if (luxon.DateTime.now().toLocaleString() === element.day) {
-                    $(`textArea[id=${idx_}]`).text(element.task);
-                }
-
-            });
+            container.empty();
 
         }
 
-    } catch (e) {
-        throw e;
-    }
+        for (let i = 0; i < time_.length; i++) {
+            let idx = -1; // get the index of the time_
+            let time_label = 0;
+            // console.log(get_working_dh.hour_12hrs_format)
 
+            if (time_.includes(getWorkDaysHours.hour_12hrs_format)) {
 
+                idx = time_.indexOf(getWorkDaysHours.hour_12hrs_format);
+            }
 
-}
+            // set the color of the rows 
+            if (i < idx) {
+                time_label = color_class[0];
 
-let persist_data = function () {
+            } else if (i == idx) {
+                time_label = color_class[1];
 
-    try {
-        if (localStorage.hasOwnProperty("schedule")) {
+            } else if (i > idx) {
+                time_label = color_class[2];
+            }
 
-            let rtv = localStorage.getItem("schedule"); // retrieve data from local store 
+            let row = $("<div/>", {
+                class: "row no-gutters",
+                id: "row_animate"
+            }).appendTo(container);
 
-            return JSON.parse(rtv);// convert from JSON to object 
+            $("<label/>", {
+                class: "hour",
+                for: "hour",
+                id: `label-${i}`,
+                text: `${time_[i]}`
+
+            }).appendTo(row);
+
+            $("<textarea/>", {
+                class: `${time_label} description`,
+                id: `${i}`,
+                name: `${i}`
+
+            }).appendTo(row);
+
+            $("<button/>", {
+                class: `saveBtn ${i}`,
+                id: `${i}`,
+                html: `<i class="bi bi-save"></i>`
+
+            }).appendTo(row);
 
         }
-        return null;
 
-    } catch (e) {
+        try {
+            let pst_data = persist_data();
 
-        throw e;
+            if (pst_data !== null) {
+
+                pst_data.forEach((element, k) => {
+
+                    let idx_ = time_.indexOf(element.time);
+
+                    // console.log(luxon.DateTime.now().toLocaleString(), element.day)
+
+                    if (luxon.DateTime.now().toLocaleString() === element.day) {
+                        $(`textArea[id=${idx_}]`).text(element.task);
+                    }
+
+                });
+
+            }
+
+        } catch (e) {
+            throw e;
+        }
     }
 
-}
+    let persist_data = function () {
 
-let save_localStorage = function (newArray) {
-    // debugger;
+        try {
+            if (localStorage.hasOwnProperty("schedule")) {
 
-    try {
-        localStorage.setItem("schedule", JSON.stringify(newArray));
+                let rtv = localStorage.getItem("schedule"); // retrieve data from local store 
 
-    } catch (e) {
-        throw e;
+                return JSON.parse(rtv);// convert from JSON to object 
+
+            }
+            return null;
+
+        } catch (e) {
+
+            throw e;
+        }
+
     }
 
-}
+    let saveToLocalStorage = function (newArray) {
+        // debugger;
 
-/*
-@brief: this retrieve_save_localStorage function, takes object (obj) as a parameter, 
-        then create temporary empty array using Array object.
+        try {
+            localStorage.setItem("schedule", JSON.stringify(newArray));
 
-        hasOwnProperty() method will check  whether "schedule"  key is existing
-        or not. If the key is there, then, retrieve the data parse to JSON,
-        then elements will push to the array using forEach() function.
+        } catch (e) {
+            throw e;
+        }
 
-        obj 
-
-*/
-
-let retrieve_save_localStorage = function (obj) {
-
-    // debugger;
-    try {
-
-        let tmp = new Array(); // create tmp array to store data 
-
-        if (localStorage.hasOwnProperty("schedule")) { // check if key: schedule exist
-
-            let rtv = localStorage.getItem("schedule"); // retrieve data from local store         
-
-            let parse_rtv = JSON.parse(rtv);// convert from JSON to object 
-
-            parse_rtv.forEach(element => {
-                tmp.push(element); // append to tmp array     
-            });
-
-        };
-        //  then push new object 
-        tmp.push(obj); // append to tmp array 
-
-        save_localStorage(tmp); // save tmp object  
-
-    } catch (e) {
-        throw e;
     }
-}
 
+    /*
+    @brief: this retrieveAndSaveToLocalStorage function, takes object (obj) as a parameter, 
+            then create temporary empty array using Array object.
+    
+            hasOwnProperty() method will check  whether "schedule"  key is existing
+            or not. If the key is there, then, retrieve the data parse to JSON,
+            then elements will push to the array using forEach() function.
+    
+            new obj will push to the array.  
+    
+    */
 
-$(document).ready(function () {
+    function retrieveAndSaveToLocalStorage(obj) {
+
+        // debugger;
+        try {
+
+            let tmp = new Array(); // create tmp array to store data 
+
+            if (localStorage.hasOwnProperty("schedule")) { // check if key: schedule exist
+
+                let rtv = localStorage.getItem("schedule"); // retrieve data from local store         
+
+                let parse_rtv = JSON.parse(rtv); // convert from JSON to object 
+
+                parse_rtv.forEach(element => {
+                    tmp.push(element); // append to tmp array     
+                });
+
+            };
+            //  then push new object 
+            tmp.push(obj); // append to tmp array 
+
+            saveToLocalStorage(tmp); // save tmp object  
+
+        } catch (e) {
+            throw e;
+        }
+    }
+
     // display current time 
 
     let prev_timer = -Infinity
@@ -256,7 +245,7 @@ $(document).ready(function () {
                 // run set_time_blocks() function every hour  
                 if (prev_timer !== luxon.DateTime.now().hour) {
                     // console.log(luxon.DateTime.now().hour, prev_timer)
-                    set_time_blocks();
+                    createTimeBlocks();
                     // update the prev_timer to current hour 
                     prev_timer = luxon.DateTime.now().hour;
 
@@ -265,18 +254,13 @@ $(document).ready(function () {
             }, 1000);
     }
 
-    // start main app.
-    startApp();
-
-
-
     $(document).on('click', '.saveBtn', function (e) {
 
         e.preventDefault();
 
         let task_ = $(`#${this.id}`).val().trim(); // get the textarea val and remove white space 
 
-        
+
         if (task_.length > 0) { // check the input val
 
             let time_label = $(`#label-${this.id}`).text().trim();
@@ -287,9 +271,12 @@ $(document).ready(function () {
                 task: task_
             }
 
-            retrieve_save_localStorage(obj)
+            retrieveAndSaveToLocalStorage(obj)
         }
 
     });
+
+    // start main app.
+    startApp();
 
 })
