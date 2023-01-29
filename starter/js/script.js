ca13a0110ff5f3e6c3c7ceba45eb5f79a8cfa34d
niwantha33 <niwantha33@gmail.com>
2023-01-29 00:11:15 +0000

    added dynamic rows.

--- starter/js/script.js
+++ starter/js/script.js
@@ -29,10 +29,10 @@ let get_working_dh = function () {
     // create object to handle working days and hours 
     let workDaysHours = new Map()
 
-    let get_days  = function () {
+    let get_days = function () {
         // splitting the string to separate day and 24 hour
         let _day = luxon.DateTime.now().toFormat('EEE, HH:a').split(',')
-        
+
         workDaysHours = {
             // check the day 
             dayCat: ['Sat', 'Sun'].includes(_day[0]) ? 'weekend' : 'week',
@@ -50,13 +50,13 @@ let get_working_dh = function () {
                 // convert the 24hrs hour to 12 
                 if (_hour > 12) {
 
-                    tmp_hour = _hour -12; // 
+                    tmp_hour = _hour - 12; // 
 
-                }else{
-                    tmp_hour = _hour; 
+                } else {
+                    tmp_hour = _hour;
                 }
-               
-                return tmp_hour+ _t[1]; 
+
+                return tmp_hour + _t[1];
 
             }, // 24hrs        
         }
@@ -67,68 +67,74 @@ let get_working_dh = function () {
 }();
 
 let set_time_blocks = function () {
+    let time_ = new Array(10);
 
-    /*
+    time_ = [
+        "7AM",
+        "8AM",
+        "9AM",
+        "10AM",
+        "11AM",
+        "0AM",
+        "1PM",
+        "2PM",
+        "3PM",
+        "4PM",
+        "5PM",
+    ]
 
-       <div class="row justify-content-md-center">
-      
-      <!-- <div class="col"> -->
-        
-      <!-- </div> -->
-      <!-- <div class="col-10"> -->
-        
-      <!-- </div> -->
-      <!-- <div class="col-1"> -->
-        <button class="saveBtn"><i class="bi bi-save"></i></button>
-      <!-- </div> -->
-  
+    let color_class = ['past', 'present', 'future'];
 
 
-
-
-    */
-
-      let time_label = 'present'
-      let timeFrame = "12PM"
+    // let timeFrame = "12PM"
     let container = $('.container');
-for(let i =0; i < 8; i++){
-    // add bootstrap  <div class="row justify-content-md-center">
-    let row = $("<div/>", {
-        class:"row no-gutters",
-        id: "row_animate"
-    }).appendTo(container); 
+    for (let i = 0; i < time_.length; i++) {
+        let idx = -1;
+        let time_label = 0;
 
-    // let column = $("<div/>", {
-    //     class:"col-6 col-md-4",
-    //     id: "row_animate"
-    // }).appendTo(row); 
+        if (time_.includes(get_working_dh.hour_12hrs_format)) {
 
-    let label = $("<label/>", {
-        class: "hour",
-        for:"hour" ,
-        text:`${timeFrame}` 
+            console.log(idx)
+            // 
+            idx = time_.indexOf(get_working_dh.hour_12hrs_format);
+            console.log(idx)
+        }
+        if (i < idx) {
+            time_label = color_class[0];
 
-    }).appendTo(row);
-   
-    // let column2 = $("<div/>", {
-    //     class:"col-6 col-md-4",
-    //     id: "row_animate"
-    // }).appendTo(row); 
+        } else if (i == idx) {
+            time_label = color_class[1];
 
-    let textArea = $("<textarea/>", {
-        class: `${time_label} description`,
-        id: "hour" 
-    }).appendTo(row);   
-    
+        } else if (i > idx) {
+            time_label = color_class[2];
+        }
 
 
-    let button = $("<button/>", {
-        class: `saveBtn`,
-        // html:`<i class="bi bi-save"></i>`
-        
-    }).appendTo(row);   
 
-}
+        let row = $("<div/>", {
+            class: "row no-gutters",
+            id: "row_animate"
+        }).appendTo(container);
+
+        let label = $("<label/>", {
+            class: "hour",
+            for: "hour",
+            text: `${time_[i]}`
+
+        }).appendTo(row);
+
+        let textArea = $("<textarea/>", {
+            class: `${time_label} description`,
+            id: "hour"
+        }).appendTo(row);
+
+        let button = $("<button/>", {
+            class: `saveBtn`,
+            html: `<i class="bi bi-save"></i>`
+
+        }).appendTo(row);
+
+    }
 
 }
 
